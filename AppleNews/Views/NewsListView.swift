c8d8571639fe98569e29b9d484ffdcbb347e883f
Russell Gordon <rgordon@lcs.on.ca>
2023-05-23 11:39:35 -0400

    Re-organized project to create Views group and NewListView.

---
+++ AppleNews/Views/NewsListView.swift
@@ -0,0 +1,20 @@
+//
+//  NewsListView.swift
+//  AppleNews
+//
+//  Created by Russell Gordon on 2023-05-23.
+//
+
+import SwiftUI
+
+struct NewsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewsListView()
+    }
+}
